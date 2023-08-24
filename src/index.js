81ba884b8a31607e14c191b19f46291515603087
vadim010975 <vadim010975@mail.ru>
2023-08-24 20:29:17 +0300

    EMS v1.0

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+import Character from './js/domain.js';
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './js/game.js';
+import { startGame } from './js/app.js';
+
+startGame();
